bbe9c7adfed8353655014c6aaa11fd8a7a9971ad
hp <JRobera>
2023-07-25 16:27:25 +0300

    fix cors two

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,6 +1,8 @@
 const getProjects = async (req, res) => {
   await Project.find({}).then((response) => {
-    res.json(response);
+    res
+      .set("Access-Control-Allow-Origin", "https://robera-abera.onrender.com")
+      .json(response);
   });
 };
 
